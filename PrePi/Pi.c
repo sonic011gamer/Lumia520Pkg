d576910bf6c515332e7903e033d4e7bf36aa4bfe
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2021-12-20 19:00:04 +0200

    Revert "Move framebuffer to the top of memory" and add Framebuffer address.

--- PrePi/Pi.c
+++ PrePi/Pi.c
@@ -44,11 +44,9 @@
 
 #ifndef _FB_ADDRESS_
 #define _FB_ADDRESS_
-#define FB_ADDR                 0x400000
+#define FB_ADDR                 0x80400000
 #endif
 
-#define FB_ADDR_REG             0xFD990008 //0xFD901E14 
-#define FB_NEW_ADDR             FixedPcdGet32(PcdMipiFrameBufferAddress)
 
 UINT64 mSystemMemoryEnd = FixedPcdGet64(PcdSystemMemoryBase) +
                           FixedPcdGet64(PcdSystemMemorySize) - 1;
@@ -60,26 +58,14 @@ UartInit
 )
 {
 
-    // Move Framebuffer to the top
-    MmioWrite32(FB_ADDR_REG,FB_NEW_ADDR);
-    // Flush using CTL0_FLUSH and Flush VIG0
-    // MmioWrite32(0xfd900618,0x00000001);
-    // MmioWrite32(0xfd900718,0x00000001); 
-
-
     SerialPortInitialize();
-    DEBUG ((EFI_D_ERROR, "\nTianoCore on Microsoft Lumia 535 (ARM)\n"));
+    DEBUG ((EFI_D_ERROR, "\nTianoCore on Microsoft Lumia 520 (ARM)\n"));
     DEBUG ((EFI_D_ERROR,  "Firmware version %s built %a %a\n\n",
 	        (CHAR16*) PcdGetPtr(PcdFirmwareVersionString),
 			        __TIME__,
 				__DATE__
 	));
 
-    DEBUG((
-        EFI_D_INFO | EFI_D_LOAD,
-        "SRC0_addrs at 0x%p\n",
-        MmioRead32(0xFD901EA4)
-  )); 
 }
 
 VOID
