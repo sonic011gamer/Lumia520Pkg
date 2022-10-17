0f1dbf60d9c74861d1d6032c01fd175496004839
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2022-10-17 21:28:30 +0300

    I've no clue what I am doing

--- Library/MsmPM8921Lib/qcom_pm8921_rtc.c
+++ Library/MsmPM8921Lib/qcom_pm8921_rtc.c
@@ -283,7 +283,10 @@ int rtc_month_days(unsigned int month, unsigned int year)
 {
 	return rtc_days_in_month[month] + (is_leap_year(year) && month == 1);
 }
-struct rtc_time *tm;
+
+typedef struct rtc_time {
+    struct rtc_time *tm;
+} rtc_time;
 void rtc_time_to_tm(unsigned int time, struct rtc_time *tm)
 {
 	unsigned int month, year;
