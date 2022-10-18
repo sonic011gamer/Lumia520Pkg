4d2931cbe8cde01fdd0da57d3653122bb26374db
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2022-10-18 19:33:21 +0300

    attempt to fix assert

--- Library/MsmTargetTimerLib/MsmTargetTimerLib.c
+++ Library/MsmTargetTimerLib/MsmTargetTimerLib.c
@@ -267,26 +267,45 @@ GetPerformanceCounter (
   return ArmGenericTimerGetSystemCount ();
 }
 
+/**
+  Retrieves the 64-bit frequency in Hz and the range of performance counter
+  values.
+  If StartValue is not NULL, then the value that the performance counter starts
+  with immediately after is it rolls over is returned in StartValue. If
+  EndValue is not NULL, then the value that the performance counter end with
+  immediately before it rolls over is returned in EndValue. The 64-bit
+  frequency of the performance counter in Hz is always returned. If StartValue
+  is less than EndValue, then the performance counter counts up. If StartValue
+  is greater than EndValue, then the performance counter counts down. For
+  example, a 64-bit free running counter that counts up would have a StartValue
+  of 0 and an EndValue of 0xFFFFFFFFFFFFFFFF. A 24-bit free running counter
+  that counts down would have a StartValue of 0xFFFFFF and an EndValue of 0.
+  @param  StartValue  The value the performance counter starts with when it
+                      rolls over.
+  @param  EndValue    The value that the performance counter ends with before
+                      it rolls over.
+  @return The frequency in Hz.
+**/
 UINT64
 EFIAPI
 GetPerformanceCounterProperties (
-  OUT UINT64  *StartValue,  OPTIONAL
-  OUT UINT64  *EndValue     OPTIONAL
+  OUT      UINT64                    *StartValue,  OPTIONAL
+  OUT      UINT64                    *EndValue     OPTIONAL
   )
 {
-	DEBUG ((EFI_D_ERROR, "TimerLib:GetPerformanceCounterProperties need mpl!!!\n"));
-	ASSERT(FALSE);
-	if (StartValue != NULL) {
-	// Timer starts with the reload value
-	//*StartValue = (UINT64)MmioRead32 (TimerBase(PcdGet32(PcdOmap35xxFreeTimer)) + GPTIMER_TLDR);
-	}
+  if (StartValue != NULL) {
+    // Timer starts at 0
+    *StartValue = (UINT64)0ULL ;
+  }
 
-	if (EndValue != NULL) {
-		// Timer counts up to 0xFFFFFFFF
-		*EndValue = 0xFFFFFFFF;
-	}
-	return PcdGet64(PcdEmbeddedPerformanceCounterFrequencyInHz);
+  if (EndValue != NULL) {
+    // Timer counts up.
+    *EndValue = 0xFFFFFFFFFFFFFFFFUL;
+  }
+
+  return (UINT64)ArmGenericTimerGetTimerFreq ();
 }
+
 
 /**
   Converts elapsed ticks of performance counter to time in nanoseconds.
